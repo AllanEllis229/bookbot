9b23ff99065e02abbd2335fc2ae08a781e827883
AllanEllis229 <allanellis229@gmail.com>
2024-10-19 08:51:50 +0100

    output added

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = book_words(text)
-    #print(f"{word_count} words found in the document")
     letters_count = book_letters(text)
-    print(letters_count)
+    count_dict = dict_sort(letters_count)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+
+    for item in count_dict:
+        if not item['char'].isalpha():
+            continue
+        print(f"The '{item['char']}' chracter was found {item['count']} times")
+    
+    print("--- End report ---")
 
 
 def get_book_text(path):  
@@ -18,11 +27,27 @@ def book_words(text):
 def book_letters(text):
     letters = {}
     for l in text.lower():
-        if l in letters:
-            letters[l] += 1
-        else:
-            letters[l] = 1
+        if l.isalpha():
+            if l in letters:
+                letters[l] += 1
+            else:
+                letters[l] = 1
     return letters
+
+def dict_sort(letters_count):
+    char_dict = letters_count
+    list_of_dicts = []
+
+    for char, count in letters_count.items():
+        new_dict = {'char': char, 'count': count}
+        list_of_dicts.append(new_dict)
+
+    def sort_on(item):
+        return item['count']
+
+    list_of_dicts.sort(reverse=True, key=sort_on)
+
+    return(list_of_dicts)
 
             
         
